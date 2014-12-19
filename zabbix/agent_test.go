af05baa1632f05feefa65a8fac544ac3a9d77b4a
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2014-12-19 13:04:39 +0900

    refactor test

--- zabbix/agent_test.go
+++ zabbix/agent_test.go
@@ -10,17 +10,13 @@ import (
 	"github.com/fujiwara/go-zabbix-get/zabbix"
 )
 
+var (
+	done    chan (bool)
+	timeout = 3 * time.Second
+)
+
 func TestAgent(t *testing.T) {
-	done := make(chan bool)
-	timeout := 3 * time.Second
-
-	{
-		value, err := zabbix.Get("localhost", "agent.ping", timeout)
-		if err == nil {
-			t.Errorf("agent is not runnig, but not error value:", value)
-		}
-	}
-
+	done = make(chan bool)
 	go zabbix.RunAgent("localhost", func(key string) (string, error) {
 		switch key {
 		case "agent.ping":
@@ -42,48 +38,57 @@ func TestAgent(t *testing.T) {
 		}
 	})
 	time.Sleep(1 * time.Second)
+}
 
-	{
-		value, err := zabbix.Get("localhost", "agent.ping", timeout)
-		if err != nil {
-			t.Error("get agent.ping failed", err)
-		}
-		if value != "1" {
-			t.Error("agent.ping value expected: 1, got:", value)
-		}
+func TestAgentCannotConnect(t *testing.T) {
+	value, err := zabbix.Get("localhost:10049", "agent.ping", timeout)
+	if err == nil {
+		t.Errorf("agent is not runnig, but not error value:", value)
 	}
+}
 
-	{
-		value, err := zabbix.Get("localhost", "agent.uptime", timeout)
-		if err != nil {
-			t.Error("get agent.ping failed", err)
-		}
-		if value != "123" {
-			t.Error("agent.uptime value expected: 123, got:", value)
-		}
+func TestAgentGetPing(t *testing.T) {
+	value, err := zabbix.Get("localhost", "agent.ping", timeout)
+	if err != nil {
+		t.Error("get agent.ping failed", err)
 	}
-
-	{
-		value, err := zabbix.Get("localhost", "xxx", timeout)
-		if err != nil {
-			t.Error("xxx failed", err)
-		}
-		if value != zabbix.ErrorMessage {
-			t.Error("xxx value expected: ", zabbix.ErrorMessage, "got:", value)
-		}
+	if value != "1" {
+		t.Error("agent.ping value expected: 1, got:", value)
 	}
+}
 
-	{
-		_, err := zabbix.Get("localhost", "timeout", timeout)
-		if err == nil {
-			t.Error("timeout must be timeouted.", err)
-		}
-		if _err := err.(*net.OpError); !_err.Timeout() {
-			t.Error("err expected i/o timeout. got:", err)
-		}
-		log.Println("client timeout")
+func TestAgentGetUptime(t *testing.T) {
+	value, err := zabbix.Get("localhost", "agent.uptime", timeout)
+	if err != nil {
+		t.Error("get agent.ping failed", err)
 	}
+	if value != "123" {
+		t.Error("agent.uptime value expected: 123, got:", value)
+	}
+}
 
+func TestAgentGetError(t *testing.T) {
+	value, err := zabbix.Get("localhost", "xxx", timeout)
+	if err != nil {
+		t.Error("xxx failed", err)
+	}
+	if value != zabbix.ErrorMessage {
+		t.Error("xxx value expected: ", zabbix.ErrorMessage, "got:", value)
+	}
+}
+
+func TestAgentGetTimeout(t *testing.T) {
+	_, err := zabbix.Get("localhost", "timeout", timeout)
+	if err == nil {
+		t.Error("timeout must be timeouted.", err)
+	}
+	if _err := err.(*net.OpError); !_err.Timeout() {
+		t.Error("err expected i/o timeout. got:", err)
+	}
+	log.Println("client timeout")
+}
+
+func TestAgentShutdown(t *testing.T) {
 	zabbix.Get("localhost", "shutdown", timeout)
 	<-done
 }
--- zabbix/trapper_test.go
+++ zabbix/trapper_test.go
@@ -10,20 +10,7 @@ import (
 )
 
 func TestTrapper(t *testing.T) {
-	done := make(chan bool)
-	timeout := 3 * time.Second
-
-	{
-		value, err := zabbix.Send(
-			zabbix.TrapperData{Host: "localhost", Key: "foo", Value: "bar"},
-			"localhost",
-			timeout,
-		)
-		if err == nil {
-			t.Errorf("trapper is not runnig, but not error value:", value)
-		}
-	}
-
+	done = make(chan bool)
 	go zabbix.RunTrapper("localhost", func(req zabbix.TrapperRequest) (res zabbix.TrapperResponse, err error) {
 		switch req.Request {
 		case "timeout":
@@ -40,72 +27,85 @@ func TestTrapper(t *testing.T) {
 		return res, nil
 	})
 	time.Sleep(1 * time.Second)
+}
 
-	{
-		res, err := zabbix.Send(
-			zabbix.TrapperData{Host: "localhost", Key: "foo", Value: "bar"},
-			"localhost",
-			timeout,
-		)
-		if err != nil {
-			t.Errorf("send failed", err)
-		}
-		if res.Proceeded != 1 {
-			t.Errorf("proceeded expected 1 got", res.Proceeded)
-		}
-		if res.Failed != 0 {
-			t.Errorf("failed expected 0 got", res.Failed)
-		}
-		if res.Total != 1 {
-			t.Errorf("total expected 1 got", res.Total)
-		}
+func TestTrapperCannotConnect(t *testing.T) {
+	value, err := zabbix.Send(
+		zabbix.TrapperData{Host: "localhost", Key: "foo", Value: "bar"},
+		"localhost:10049",
+		timeout,
+	)
+	if err == nil {
+		t.Errorf("trapper is not runnig, but not error value:", value)
 	}
+}
 
-	{
-		res, err := zabbix.SendBulk(
-			zabbix.TrapperRequest{
-				Data: []zabbix.TrapperData{
-					zabbix.TrapperData{Host: "localhost", Key: "foo", Value: "bar"},
-					zabbix.TrapperData{Host: "localhost", Key: "xxx", Value: "yyy"},
-				},
+func TestTrapperSend(t *testing.T) {
+	res, err := zabbix.Send(
+		zabbix.TrapperData{Host: "localhost", Key: "foo", Value: "bar"},
+		"localhost",
+		timeout,
+	)
+	if err != nil {
+		t.Errorf("send failed", err)
+	}
+	if res.Proceeded != 1 {
+		t.Errorf("proceeded expected 1 got", res.Proceeded)
+	}
+	if res.Failed != 0 {
+		t.Errorf("failed expected 0 got", res.Failed)
+	}
+	if res.Total != 1 {
+		t.Errorf("total expected 1 got", res.Total)
+	}
+}
+
+func TestTrapperSendBulk(t *testing.T) {
+	res, err := zabbix.SendBulk(
+		zabbix.TrapperRequest{
+			Data: []zabbix.TrapperData{
+				zabbix.TrapperData{Host: "localhost", Key: "foo", Value: "bar"},
+				zabbix.TrapperData{Host: "localhost", Key: "xxx", Value: "yyy"},
 			},
-			"localhost",
-			timeout,
-		)
-		if err != nil {
-			t.Errorf("send failed", err)
-		}
-		if res.Proceeded != 2 {
-			t.Errorf("proceeded expected 2 got", res.Proceeded)
-		}
-		if res.Failed != 0 {
-			t.Errorf("failed expected 0 got", res.Failed)
-		}
-		if res.Total != 2 {
-			t.Errorf("total expected 2 got", res.Total)
-		}
+		},
+		"localhost",
+		timeout,
+	)
+	if err != nil {
+		t.Errorf("send failed", err)
 	}
+	if res.Proceeded != 2 {
+		t.Errorf("proceeded expected 2 got", res.Proceeded)
+	}
+	if res.Failed != 0 {
+		t.Errorf("failed expected 0 got", res.Failed)
+	}
+	if res.Total != 2 {
+		t.Errorf("total expected 2 got", res.Total)
+	}
+}
 
-	{
-		res, err := zabbix.SendBulk(
-			zabbix.TrapperRequest{
-				Request: "timeout",
-				Data: []zabbix.TrapperData{
-					zabbix.TrapperData{Host: "localhost", Key: "foo", Value: "bar"},
-				},
+func TestTrapperSendTimeout(t *testing.T) {
+	res, err := zabbix.SendBulk(
+		zabbix.TrapperRequest{
+			Request: "timeout",
+			Data: []zabbix.TrapperData{
+				zabbix.TrapperData{Host: "localhost", Key: "foo", Value: "bar"},
 			},
-			"localhost",
-			timeout,
-		)
-		if err == nil {
-			t.Error("timeout must be timeouted.", err)
-		}
-		if _err := err.(*net.OpError); !_err.Timeout() {
-			t.Error("err expected i/o timeout. got:", err)
-		}
-		log.Println("client timeout", res)
+		},
+		"localhost",
+		timeout,
+	)
+	if err == nil {
+		t.Error("timeout must be timeouted.", err)
 	}
+	if _err := err.(*net.OpError); !_err.Timeout() {
+		t.Error("err expected i/o timeout. got:", err)
+	}
+	log.Println("client timeout", res)
+}
 
+func TestTrapperShutdown(t *testing.T) {
 	zabbix.SendBulk(
 		zabbix.TrapperRequest{Request: "shutdown"},
 		"localhost",
